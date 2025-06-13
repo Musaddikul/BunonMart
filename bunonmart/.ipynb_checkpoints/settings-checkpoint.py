884edd203d0471470a4319531952ea138bf81efb
Musaddikul <musaddikul.amin123@gmail.com>
2025-06-13 21:18:53 +0600

    updated

--- bunonmart/.ipynb_checkpoints/settings-checkpoint.py
+++ bunonmart/.ipynb_checkpoints/settings-checkpoint.py
@@ -96,25 +96,23 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "bunonmart.wsgi.application"
-
 # Database
 # https://docs.djangoproject.com/en/stable/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bunonmart_db',
-        'USER': 'postgres',
-        'PASSWORD': 'mugdho22',
-        'HOST': 'localhost',
-        'PORT': '5252',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bunonmart_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'mugdho22',
+#         'HOST': 'localhost',
+#         'PORT': '5252',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
